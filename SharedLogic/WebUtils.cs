699dea4ae099ace89d0029a31778d8d6539070fb
schloum <schloum@UCF12816>
2017-02-27 21:54:40 +0100

    Ajoutez des fichiers projet.

---
+++ SharedLogic/WebUtils.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Xml;
+
+namespace SharedLogic
+{
+    public static class WebUtils
+    {
+       public static string XmlToJson (string xmlInput)
+        {
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.LoadXml(xmlInput);
+
+            } catch (XmlException)
+            {
+                return "Bad Xml format";
+            }
+
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+
+            return jsonText;
+        }
+    }
+}
